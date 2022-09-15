826075f90dae8e5c76406064d42ea64b1a0fb014
12nasir <nasirahmed8202@gmail.com>
2022-09-15 08:49:40 +0600

    clear input

--- phone.js
+++ phone.js
@@ -1,18 +1,19 @@
 const searchPhone = () =>{
     const searchInput = document.getElementById('search-text')
     const searchText = searchInput.value;
-    // console.log(searchText)
     searchInput.value = '';
     fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`) 
     .then(res => res.json())
     .then(data => displayLoadPhone(data.data))
 }
+searchPhone('phone');
 
+// display load phone
 const displayLoadPhone = phones =>{
-    // console.log(phones.data)
     const sectionContainer = document.getElementById('section-container')
-    phones.forEach(phone =>{
-        console.log(phone)
+    sectionContainer.textContent = '';
+    phones?.forEach(phone =>{
+        // console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col')
         div.style.margin = '15px'
@@ -20,22 +21,49 @@ const displayLoadPhone = phones =>{
         div.style.width = '325px'
         div.style.border = '1px solid orange';
         div.style.borderRadius = '5px';
-        // div.style.backgroundColor = 'blue'
         div.innerHTML = `
           <img src="${phone.image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title text-success">${phone.brand}</h5>
             <h4 class="card-text text-primary">${phone.phone_name}</h4>
-            <button type="button" class="btn btn-success">See details</button>
+            <button onclick="loadPhoneDetails('${phone?.slug}')" type="button" class="btn btn-success">See details</button>
 
           </div>
         `;
         sectionContainer.appendChild(div)
     })
-
 }
 
-searchPhone('phone');
+// Phone details
+const loadPhoneDetails = phoneName =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneName}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayDetails(data.data))
+}
 
+const displayDetails = phone =>{
+  console.log(phone)
+  const details = document.getElementById('phone-details');
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <img src="${phone.image}" class="card-img-top" width='100px' alt="...">
+  <ul >
+    <h4 class="card-title">${phone.name}</h4>
+    <h6>${phone.releaseDate ? phone.releaseDate :'Release date not found'}</h6>
+    <h6 ">${phone.others.Bluetooth }</h6>
+    <h6 class="list-group-item">${phone.others.GPS}</h6>
+    <h6 class="list-group-item">${phone.others.GPS}</h6>
+    <h6 class="list-group-item">${phone.others.USB}</h6>
+    <h6 class="list-group-item">Sensors Info: ${phone.mainFeatures.sensors[0]}</h6>
+    <h6 class="list-group-item">Sensors Info: ${phone.mainFeatures.sensors[1]}</h6>
+    <h6 class="list-group-item">Sensors info: ${phone.mainFeatures.sensors[2]}</h6>
+  </ul>
+  `;
+  details.appendChild(div)
+}
+
+
+// class="list-group list-group-flush"
 
 
